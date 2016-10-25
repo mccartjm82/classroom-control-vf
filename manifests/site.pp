9bf3517a3b38ce559d699b5f1cb30da1eb082a0e
mccartjm82 <mccartjm@hotmail.com>
2016-10-25 14:08:53 -0500

    default etc/motd file

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  file { '/etc/motd':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0755'
+    content => "Automate all the things\n"
+  }
 }
