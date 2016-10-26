a304c33cfc106bdbceee2c90b5df51621f053486
Jeremy McCartney <mccartjm@hotmail.com>
2016-10-26 14:00:38 -0500

    fix site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,7 +37,7 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-node 'mccartjm.puppetlabs.vm'{
+node 'mccartjm82.puppetlabs.vm'{
   include memcached
 }
 
