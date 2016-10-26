134134db8035a41406b8dd80f834c04a21bb0d53
Jeremy McCartney <mccartjm@hotmail.com>
2016-10-26 13:55:42 -0500

    add comma

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -15,7 +15,6 @@ class memcached {
   service { 'memcached':
     ensure => running, 
     enable => true, 
-    subscribe => File['/etc/sysconfig/memcached']
-
+    subscribe => File['/etc/sysconfig/memcached'],
   }
 }
