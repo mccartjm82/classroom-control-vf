b219978177fb6487bcc2ef540dbfa8ef2393ee20
Jeremy McCartney <jmccartney@ntst.com>
2016-10-27 15:07:30 -0500

    extract platforms for nginx module

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,37 +1,70 @@
 # nginx/manifests/init.pp
 class nginx {
 
+  case $::osfamily{
+    'redhat','debian':{
+      $package = 'nginx',
+      $owner = 'root',
+      $group = 'root',
+      $docroot = 'var/www',
+      $confdir = '/etc/nginx',
+      $logdir = '/var/log/nginx',
+    }
+    'windows':{
+      $package = 'nginx',
+      $owner = 'administrator',
+      $group = 'administrator',
+      $docroot = 'C:/ProgramData/nginx/html',
+      $confdir = 'C:/ProgramData/nginx',
+      $logdir = 'C:/ProgramData/nginx/logs',
+    }
+    default :{
+      fail("Module ${module_name} is not supported on ${::osfamily}")
+    }
+  }
+
+  $user = $::osfamily ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
+
   File {
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
+    owner  => $owner,
+    group  => $group,
     mode   => '0644',
   }
 
-  package { 'nginx':
+  package { $package:
     ensure => present,
-    before => [File['/etc/nginx/conf.d/default.conf'],File['/etc/nginx/nginx.conf']],
   }
 
-  file { '/var/www':
+  file { [$docroot,"${confdir}/conf.d"]:
     ensure => directory,
   }
 
-  file { '/var/www/index.html':
+  file { "${docroot}/index.html":
     source => 'puppet:///modules/nginx/index.html',
   }
 
-  file { '/etc/nginx/conf.d/default.conf':
+  file { "${confdir}/conf.d/default.conf":
     source  => 'puppet:///modules/nginx/default.conf',
   }
 
-  file { '/etc/nginx/nginx.conf':
+  file { "${confdir}/nginx.conf":
     source  => 'puppet:///modules/nginx/nginx.conf',
+    content => epp('nginx/nginx.conf.epp',
+      {
+        user => $user,
+        confdir => $confdir,
+        logdir => $logdir,
+        }),
+    notify => Service['nginx'],
 }
 
   service { 'nginx':
     ensure    => running,
     enable    => true,
-    subscribe => [File['/etc/nginx/conf.d/default.conf'],File['/etc/nginx/nginx.conf']],
   }
 }
