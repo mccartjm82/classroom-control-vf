efb07362a3664eef1d022238b36b422df0182ec4
Jeremy McCartney <jmccartney@ntst.com>
2016-10-27 15:15:52 -0500

    remove commas

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,21 +3,21 @@ class nginx {
 
   case $::osfamily{
     'redhat','debian':{
-      $package = 'nginx',
-      $owner = 'root',
-      $group = 'root',
-      $docroot = 'var/www',
-      $confdir = '/etc/nginx',
-      $logdir = '/var/log/nginx',
-    },
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = 'var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
+    }
     'windows':{
-      $package = 'nginx',
-      $owner = 'administrator',
-      $group = 'administrator',
-      $docroot = 'C:/ProgramData/nginx/html',
-      $confdir = 'C:/ProgramData/nginx',
-      $logdir = 'C:/ProgramData/nginx/logs',
-    },
+      $package = 'nginx'
+      $owner = 'administrator'
+      $group = 'administrator'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
     default :{
       fail("Module ${module_name} is not supported on ${::osfamily}")
     }
