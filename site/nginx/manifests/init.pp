b821ff8576e8ed561cce70b61ece7436395e12ca
Jeremy McCartney <jmccartney@ntst.com>
2016-10-27 09:45:36 -0500

    refactor nginx init.pp more

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,31 +1,31 @@
 class nginx {
-  package { 'nginx':
-    ensure => present,
-  }
-
   file {
     owner => 'root',
     group => 'root',
     mode => '0644',
   }
 
-  file { ['/var/www/', '/etc/nginx/conf.d/']:
-    ensure  => directory,
-    require => Package['nginx'],
+  package { 'nginx':
+    ensure => present,
   }
-  file { '/var/www/index.html':
+
+  file {['/var/www/', '/etc/nginx/conf.d/']:
+    ensure  => directory,
+  }
+
+  file {'/var/www/index.html':
     ensure  => file,
     source  => 'puppet:///modules/nginx/index.html',
-    require => File['/var/www/'],
   }
-  file { '/etc/nginx/nginx.conf':
+
+  file {'/etc/nginx/nginx.conf':
     ensure  => file,
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
 
-  file { '/etc/nginx/conf.d/default.conf':
+  file {'/etc/nginx/conf.d/default.conf':
     ensure  => file,
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
