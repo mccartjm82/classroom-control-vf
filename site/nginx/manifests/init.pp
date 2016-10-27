effa5d0c964d1090437ce2871f57b0a96da3ab3b
Jeremy McCartney <jmccartney@ntst.com>
2016-10-27 09:39:28 -0500

    refactor nginx init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,48 +2,36 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
-  
-  file { '/var/www/':
+
+  file {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+
+  file { ['/var/www/', '/etc/nginx/conf.d/']:
     ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0775',
     require => Package['nginx'],
   }
   file { '/var/www/index.html':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
     source  => 'puppet:///modules/nginx/index.html',
     require => File['/var/www/'],
   }
-  file { '/etc/nginx/nginx.conf': 
+  file { '/etc/nginx/nginx.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0775',
     source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
-  file { '/etc/nginx/conf.d/':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0775',
-    require => Package['nginx'],
-  }
-  file { '/etc/nginx/conf.d/default.conf': 
+
+  file { '/etc/nginx/conf.d/default.conf':
     ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0664',
     source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     notify  => Service['nginx'],
   }
-  
+
   service {'nginx':
     ensure => running,
     enable => true,
