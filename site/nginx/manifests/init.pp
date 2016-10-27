98b2473acb1e36154aba5e1e1cb869c06ac78057
Jeremy McCartney <jmccartney@ntst.com>
2016-10-27 15:10:09 -0500

    add comma

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -9,7 +9,7 @@ class nginx {
       $docroot = 'var/www',
       $confdir = '/etc/nginx',
       $logdir = '/var/log/nginx',
-    }
+    },
     'windows':{
       $package = 'nginx',
       $owner = 'administrator',
@@ -17,7 +17,7 @@ class nginx {
       $docroot = 'C:/ProgramData/nginx/html',
       $confdir = 'C:/ProgramData/nginx',
       $logdir = 'C:/ProgramData/nginx/logs',
-    }
+    },
     default :{
       fail("Module ${module_name} is not supported on ${::osfamily}")
     }
