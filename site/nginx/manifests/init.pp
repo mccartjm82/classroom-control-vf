cfd26f197c8da5b669cc718dde3eda66464f7400
Jeremy McCartney <jmccartney@ntst.com>
2016-10-28 11:33:22 -0500

    update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,53 +1,56 @@
-# nginx/manifests/init.pp
-class nginx ($root = undef,){
+class nginx (
+  $root = undef,
+) {
 
-  case $::osfamily{
-    'redhat','debian':{
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      #$docroot = '/var/www'
+  case $::osfamily {
+    'RedHat','Debian': {
+#      $docroot = '/var/www'
+      $logsdir = '/var/log/nginx'
       $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-      $default_docroot '/var/www'
+      $blckdir = '/etc/nginx/conf.d'
+      $pkgname = 'nginx'
+      $fileown = 'root'
+      $filegrp = 'root'
+      $svcname = 'nginx'
+      $default_docroot = '/var/www'
     }
-    'windows':{
-      $package = 'nginx-service'
-      $owner = 'administrator'
-      $group = 'administrator'
-      #$docroot = 'C:/ProgramData/nginx/html'
+    'windows': {
+#      $docroot = 'C:/ProgramData/nginx/html'
+      $logsdir = 'C:/ProgramData/nginx/logs'
       $confdir = 'C:/ProgramData/nginx'
-      $logdir = 'C:/ProgramData/nginx/logs'
+      $blckdir = 'C:/ProgramData/nginx/conf.d'
+      $pkgname = 'nginx-service'
+      $fileown = 'Administrator'
+      $filegrp = 'Administrators'
+      $svcname = 'nginx'
       $default_docroot = 'C:/ProgramData/nginx/html'
-    }
-    default :{
-      fail("Module ${module_name} is not supported on ${::osfamily}")
     }
   }
 
-  $user = $::osfamily ? {
-    'redhat' => 'nginx',
-    'debian' => 'www-data',
+  $svcuser = $::osfamily ? {
+    'RedHat'  => 'nginx',
+    'Debian'  => 'www-data',
     'windows' => 'nobody',
   }
 
-  $docroot = $root ?{
-    undef => $default_docroot,
+  $docroot = $root ? {
+    undef   => $default_docroot,
     default => $root,
   }
 
   File {
     ensure => file,
-    owner  => $owner,
-    group  => $group,
+    owner  => $fileown,
+    group  => $filegrp,
     mode   => '0644',
   }
 
-  package { $package:
+  package { $pkgname:
     ensure => present,
+    before => [File["${blckdir}/default.conf"],File["${confdir}/nginx.conf"]],
   }
 
-  file { [$docroot,"${confdir}/conf.d"]:
+  file { $docroot:
     ensure => directory,
   }
 
@@ -55,26 +58,17 @@ class nginx ($root = undef,){
     source => 'puppet:///modules/nginx/index.html',
   }
 
-  file { "${confdir}/nginx.conf":
-    content => epp('nginx/nginx.conf.epp',
-      {
-        user => $user,
-        confdir => $confdir,
-        logdir => $logdir,
-        }),
-    notify => Service['nginx'],
-}
-
-  file { "${confdir}/conf.d/default.conf":
-    content => epp('nginx/default.conf.epp',
-      {
-        docroot => $docroot,
-      }),
-    notify => Service['nginx'],
+  file { "${blckdir}/default.conf":
+    content  => epp('nginx/default.conf.epp'),
   }
 
-  service { 'nginx':
+  file { "${confdir}/nginx.conf":
+    content  => epp('nginx/nginx.conf.epp'),
+}
+
+  service { $svcname:
     ensure    => running,
     enable    => true,
+    subscribe => [File["${blckdir}/default.conf"],File["${confdir}/nginx.conf"]],
   }
 }
