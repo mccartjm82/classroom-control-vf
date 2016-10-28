2bc6af81e76730950397b904cc2ef9118c3be44c
Jeremy McCartney <jmccartney@ntst.com>
2016-10-28 11:49:53 -0500

    add params to init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,15 @@
 # nginx/manifests/init.pp
 class nginx (
-  $root = undef,
+  $docroot = $nginx::params::docroot,
+  $logsdir = $nginx::params::logsdir,
+  $confdir = $nginx::params::confdir,
+  $blckdir = $nginx::params::blckdir,
+  $pkgname = $nginx::params::pkgname,
+  $fileown = $nginx::params::fileown,
+  $filegrp = $nginx::params::filegrp,
+  $svcname = $nginx::params::svcname,
+  $svcuser = $nginx::params::svcuser,
 ) {
-
-  case $::osfamily {
-    'RedHat','Debian': {
-#      $docroot = '/var/www'
-      $logsdir = '/var/log/nginx'
-      $confdir = '/etc/nginx'
-      $blckdir = '/etc/nginx/conf.d'
-      $pkgname = 'nginx'
-      $fileown = 'root'
-      $filegrp = 'root'
-      $svcname = 'nginx'
-      $default_docroot = '/var/www'
-    }
-    'windows': {
-#      $docroot = 'C:/ProgramData/nginx/html'
-      $logsdir = 'C:/ProgramData/nginx/logs'
-      $confdir = 'C:/ProgramData/nginx'
-      $blckdir = 'C:/ProgramData/nginx/conf.d'
-      $pkgname = 'nginx-service'
-      $fileown = 'Administrator'
-      $filegrp = 'Administrators'
-      $svcname = 'nginx'
-      $default_docroot = 'C:/ProgramData/nginx/html'
-    }
-  }
-
-  $svcuser = $::osfamily ? {
-    'RedHat'  => 'nginx',
-    'Debian'  => 'www-data',
-    'windows' => 'nobody',
-  }
-
   $docroot = $root ? {
     undef   => $default_docroot,
     default => $root,
